7ca6ed14d8e69bf5e17447dfa8fd5d27de1c8ac0
raihan ramadhan <raihanramadhan.rn22@gmail.com>
2023-11-05 16:50:44 +0700

    Add Incremental static regeneration

--- app/project/[projectId]/page.tsx
+++ app/project/[projectId]/page.tsx
@@ -1,10 +1,20 @@
-"use client";
-
-import { useParams, notFound } from "next/navigation";
 import { projectsData } from "@/lib/data";
+import { notFound } from "next/navigation";
 
-const Page: React.FC = () => {
-  const { projectId } = useParams();
+interface PageProps {
+  params: {
+    projectId: string;
+  };
+}
+
+export async function generateStaticParams() {
+  return projectsData.map((project) => ({
+    projectId: project.id,
+  }));
+}
+
+const Page: React.FC<PageProps> = ({ params }) => {
+  const { projectId } = params;
 
   const project = projectsData.find((project) => project.id == projectId);
 
@@ -14,9 +24,16 @@ const Page: React.FC = () => {
     <div className="flex-1">
       <h1>Coming Soon</h1>
       <div>{project.title}</div>
-      <a href={project.url} className="underline" target="_blank">
-        Link Repo
-      </a>
+      <p>
+        <a href={project.repo} className="underline" target="_blank">
+          Link Repo
+        </a>
+      </p>
+      <p>
+        <a href={project.url} className="underline" target="_blank">
+          Link Website
+        </a>
+      </p>
     </div>
   );
 };
