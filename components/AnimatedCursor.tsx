0848cdd8a82391402f09b9fe1d99b9f19ecaa3d5
Raihan <raihanramadhan.rn22@gmail.com>
2025-03-27 06:39:01 +0700

    solved bug AnimatedCursor in touch device

--- components/AnimatedCursor.tsx
+++ components/AnimatedCursor.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const Cursor = dynamic(() => import("react-animated-cursor"), {
   ssr: false,
 });
 
 const AnimatedCursor = () => {
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    const checkTouchDevice = () => {
+      if ("ontouchstart" in window || navigator.maxTouchPoints > 0) {
+        setIsTouchDevice(true);
+      }
+    };
+
+    checkTouchDevice();
+  }, []);
+
+  if (isTouchDevice) return null;
+
   return (
     <Cursor
       innerSize={8}
